12accd2985330386c2facbe3f48ebca1f01480a1
Эльвина Коршикова <106970196+elvina27@users.noreply.github.com>
2022-10-25 22:51:35 +0300

    Add files via upload

---
+++ Арканоид/Pictures.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Арканоид
+{
+    internal class Pictures
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Size { get; set; }
+    }
+}
